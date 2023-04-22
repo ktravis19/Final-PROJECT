8abb45479c4e12bba139b2b10e018bf440d1611e
Kathy Travis <ktravis@KT7908-2.local>
2023-04-22 12:45:50 -0400

    pushing changies

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
 console.log("testing");
 
 
-// List Items
+// Contact Form
 
-const artDirection = document.getElementById('art-direction');
-const editorialDesign = document.getElementById('editorial-design');
-const digitalMarketing = document.getElementById('digital-marketing');
-const brandIdentity = document.getElementById('brand-identity');
-const printWebDesign = document.getElementById('print-web-design');
-
-// add art direction
-artDirection.addEventListener('click', addArtDirection);
-
-function addArtDirection(){
-  artDirection.classList.add('art-direction');
- 
+const form = document.querySelector ("#contact-form");
+const firstInput = document.querySelector ("name");
+const secondInput = document.querySelector ("email");
+const thirdInput = document.querySelector ("message");
